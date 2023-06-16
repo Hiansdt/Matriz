e7623a10144dad34162321b7463631e750d6eaf8
Hiansdt <vinijao123690@gmail.com>
2023-06-16 15:37:44 -0300

    Função Para Criação das matrizes na tela - 4

--- Matrizes/src/App.vue
+++ Matrizes/src/App.vue
@@ -1,20 +1,96 @@
 <script setup>
-  import {ref} from 'vue'
+import { ref } from 'vue'
 
+const linhasMatrizEmInput = ref()
+const colunasMatrizEmInput = ref()
+
+const linhasMatriz = ref()
+const colunasMatriz = ref()
+
+const matriz = ref([])
+
+const maiorNumeroDeCadaColuna = ref([])
+
+function AtualizarTamanhoMatriz() {
+  linhasMatriz.value = linhasMatrizEmInput.value
+  colunasMatriz.value = colunasMatrizEmInput.value
+
+  for (let i = 0; i < linhasMatriz.value; i++) {
+    matriz.value.push([])
+  }
+}
+
+function calcular() {
+  console.log(maiorNumeroDeCadaColuna.value)
+
+  maiorNumeroDeCadaColuna.value = []
+
+  for (let n = 0; n < colunasMatriz.value.length; n++) {
+
+    let maiorNumero = -Infinity
+
+    for (let i = 0; i < matriz.value.length; i++) {
+      if (matriz.value[i][n] > maiorNumero) {
+        maiorNumero = matriz.value[i][n]
+      }
+    }
+
+    maiorNumeroDeCadaColuna.value.push(maiorNumero)
+  }
+}
 </script>
 
 <template>
+  <div class="all">
+    <input
+      type="number"
+      placeholder="Digite a quantidade de linhas da matriz"
+      v-model="linhasMatrizEmInput"
+    />
+    <input
+      type="number"
+      placeholder="Digite a quantidade de colunas da matriz"
+      v-model="colunasMatrizEmInput"
+    />
 
-<div class="all">
+    <br />
 
-  <input type="text" placeholder="Digite a quantidade de linhas da Matriz">
-  <input type="text" placeholder="Digite a quantidade de colunas da Matriz">
+    <span>{{ linhasMatriz }}x{{ colunasMatriz }}</span>
 
-  
+    <br />
 
-</div>
+    <button @click="AtualizarTamanhoMatriz()">Confirmar Tamanho</button>
 
+    <div class="matriz">
+      <div class="linhas" v-for="(linha, indexLinha) in linhasMatriz" :key="indexLinha">
+        <input
+          type="number"
+          v-for="(coluna, indexColuna) in colunasMatriz"
+          :key="indexColuna"
+          class="inputs"
+          v-model="matriz[indexLinha][indexColuna]"
+        />
+      </div>
+
+      <button @click="calcular()">Calcular</button>
+
+      {{ matriz }}
+
+      <br />
+
+      {{ maiorNumeroDeCadaColuna }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.inputs {
+  width: 100px;
+  height: 100px;
+}
+
+.all {
+  width: 180vh;
+  height: 80vh;
+}
 </style>
