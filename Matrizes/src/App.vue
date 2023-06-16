497f0967f076dcd7aee414e08841810333c5462e
Hiansdt <vinijao123690@gmail.com>
2023-06-16 14:21:56 -0300

    Inputs para tamanho da Matriz - 3

--- Matrizes/src/App.vue
+++ Matrizes/src/App.vue
@@ -1,36 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import {ref} from 'vue'
+
 </script>
 
 <template>
+
+<div class="all">
+
+  <input type="text" placeholder="Digite a quantidade de linhas da Matriz">
+  <input type="text" placeholder="Digite a quantidade de colunas da Matriz">
+
+  
+
+</div>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
